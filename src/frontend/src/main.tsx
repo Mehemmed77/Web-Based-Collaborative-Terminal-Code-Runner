56e26276fc9d4b340354853cb41c119325ef88fa
Mehemmed Kazimzade <mehemmedkazimzade54@gmail.com>
2026-02-03 18:07:31 +0400

    tough day, mainly drowned in configurations

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+import "./main.css";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import CreateRoom from "./rooms/CreateRoom";
 import GlobalContextProvider from "./context/ContextProvider";
 import Room from "./rooms/Room";
-import { MantineProvider } from "@mantine/core";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/frontend/src/terminalUI/Terminal.tsx
+++ src/frontend/src/terminalUI/Terminal.tsx
@@ -1,5 +1,12 @@
 export default function Terminal() {
-    return (
-        <></>
-    )
+  return (
+    <div className="max-w-[900px] border-3 border-solid border-popover-foreground rounded-md">
+      {/* <div className="">
+        <h4 className="">Transcend my humanity</h4>
+      </div> */}
+      <div>
+        
+      </div>
+    </div>
+  );
 }
